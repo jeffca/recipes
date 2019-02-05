5f43c13cd0924f61e977aa58c3d142aaf2482632
Jeff Cairns <jeffca@Jeffs-MacBook-Pro.local>
2019-02-04 17:49:45 -0800

    commit to github

--- recipes/models/__init__.py
+++ recipes/models/__init__.py
@@ -6,8 +6,6 @@ database = PostgresqlExtDatabase('d8o164eqmava48', host="ec2-75-101-142-91.compu
                               port="5432",
                               password="b347a0c9c9f5994f09746706dd7e31c14eef23aac2b5aa41b82f7c17999b6d31")
 
-
-
 class BaseModel(Model):
     class Meta:
         database=database
@@ -17,5 +15,13 @@ class Recipe(BaseModel):
     ingredients = CharField()
     instructions = CharField()
     menuimgurl = CharField()
-    categories = ArrayField()
 
+class Category(BaseModel):
+    category = CharField()
+    order = CharField()
+
+class Recipe_Category(BaseModel):
+    recipe = ForeignKeyField(Recipe)
+    category = ForeignKeyField(Category)
+    recipe_id = IntegerField()
+    category_id = IntegerField()
--- recipes/views/aview.py
+++ recipes/views/aview.py
@@ -1,6 +1,6 @@
 from recipes import app
 from flask import Flask, render_template, request, redirect, url_for, flash, Response, jsonify
-from recipes.models import Recipe
+from recipes.models import *
 # from stretching.database import db_session
 # import datetime 	
 # from datetime import timedelta
@@ -12,16 +12,46 @@ from recipes.models import Recipe
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	recipes = Recipe.select()
+	categories = Category.select().order_by(Category.order)
+	return render_template('index.html', categories=categories)
 
-@app.route('/categories')
-def categories():
-    categories = []
-    for recipe in Recipe.select():
-        if (recipe.categories != None):
-            for category in recipe.categories:
-                categories.append(category)
-    return jsonify(list(set(categories)))
+@app.route("/newCategoryMapping", methods=['GET', 'POST'])
+def newCategoryMapping():
+	if request.method == 'POST':
+		print(request.form.getlist('category'))
+		for c in request.form.getlist('category'):
+			Recipe_Category.create(recipe_id = request.form.get('recipe'), 
+				category_id = c)
+		return home()
+	else:
+		recipes = Recipe.select()
+		categories = Category.select().order_by(Category.order)
+		return render_template("newCategoryMapping.html", recipes=recipes, categories=categories)
+
+@app.route("/newRecipe", methods=["GET", "POST"])
+def newRecipe():
+	if request.method == 'GET':
+		return render_template("newRecipe.html")
+	else:
+		Recipe.create(recipe=request.form.get('name'), 
+			ingredients=request.form.get('ingredients'), 
+			instructions=request.form.get('instructions'), 
+			menuimgurl=request.form.get('imageurl'))
+		return home()
+
+@app.route("/category/<category_id>")
+def categorydetail(category_id):
+	recipes = Recipe.select().join(Recipe_Category).join(Category).where(Recipe_Category.category_id == category_id)
+	category = Category.select().where(Category.id == category_id).first()
+	return render_template("category.html", recipes=recipes, category=category)
+
+@app.route("/category/<category_id>/recipe/<recipe_id>")
+def detail(category_id, recipe_id):
+	recipe = Recipe.select().where(Recipe.id == recipe_id).first()
+	category = Category.select().where(Category.id == category_id).first()
+	return render_template('recipe.html', recipe=recipe, category=category)
+
 
 @app.route('/recipes')
 def recipes():
